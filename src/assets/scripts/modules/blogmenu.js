653ce22c446aeb73024bf20c673021a36a16217b
Alexander Varnaev <alexandervarnaev@gmail.com>
2018-10-11 17:53:15 +0300

    blogmenu

--- src/assets/scripts/modules/blogmenu.js
+++ src/assets/scripts/modules/blogmenu.js
@@ -9,8 +9,10 @@ for (let i = 0; i < btns.length; i++) {
   button.onclick = function () {
     //При клике на кнопку определяем data id и родительский элемент,
     //Удаляем все активные элементы у списков и добавляем активный класс по клику
+
     let data_id = this.dataset.id;
     let parent = this.parentNode;
+    
     for (let i = 0; i < items.length; i++) {
       const item = items[i];
       item.classList.remove("blog-menu__item--active");
@@ -20,17 +22,11 @@ for (let i = 0; i < btns.length; i++) {
     for (let i = 0; i < posts.length; i++) {
       const post = posts[i];
 
-      let postHeight = post.clientHeight;
       if (post.dataset.id === data_id) {
-        // console.log(post.offsetTop + " Позиция поста");
-        // window.onscroll = function () {
-        //   console.log(postPosition);
 
-        // }
         window.scrollTo({
-          //определяем top поста и высоту поста
-          //Складываем и прокручиваем
-          top: post.offsetTop + postHeight,
+          //определяем top поста и высоту поста и прокручиваем
+          top: post.offsetTop,
           behavior: "smooth"
         })
       }
